950228aebec8ae5eebd505c5f040a7681d252398
harshitkarn4444 <135310103+harshitkarn4444@users.noreply.github.com>
2024-07-15 21:22:35 +0530

    Add files via upload

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const {setCurrency} =useContext(CoinContext)
+
+    const currencyHandler=(event)=>{
+        switch (event.target.value){
+            case "usd":{
+                setCurrency({name:"usd",symbol:"$"});
+                break;
+            }
+            case "inr":{
+                setCurrency({name:"inr",symbol:"₹"});
+                break;
+            }
+            case "eur":{
+                setCurrency({name:"eur",symbol:"€"});
+                break;
+            }
+            default : {
+                setCurrency({name:"usd",symbol:"$"});
+                break;
+            }
+        }
+    }
+
+  return (
+    <div className='navbar'>
+        <Link to={`/`}><img src={logo} alt="" className='logo'/></Link>
+        <ul>
+        <Link to={`/`}><li>Home</li></Link>
+        <Link to={`/coin/bitcoin`}><li>Bitcoin</li></Link>
+        <Link to={`/coin/ethereum`}><li>Ethereum</li></Link>
+        <Link to={`/coin/tether`}><li>Tether</li></Link>
+        </ul>
+        <div className="nav-right">
+            <select onChange={currencyHandler}>
+                <option value="usd">USD</option>
+                <option value="inr">INR</option>
+                <option value="eur">EUR</option>
+            </select>
+            <button>Sign Up <img src={arrow_icon} alt="" /></button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
